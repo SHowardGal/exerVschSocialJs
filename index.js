bb68cc5e4094e16bdc81322356f3543f9b4fde06
SHowardGal <u0250173@utah.edu>
2021-12-01 11:56:48 -0700

    commit

---
+++ index.js
@@ -0,0 +1,59 @@
+var mountainClimber = {
+    name: "Bob",
+    age: 31,
+    favoriteClimbs: [],
+    mountainClimbingFriends: [
+        {
+            name: "Sarah",
+            age: 31,
+            favoriteClimbs: [
+                {
+                    climbName: "Mount Everest",
+                    difficulty: 10
+                },
+            ]
+        },
+        {
+            name: "Mike",
+            age: 30,
+            favoriteClimbs: [
+                {
+                    climbName: "K2",
+                    difficulty: 10
+                },
+            ]
+        },
+        {
+            name: "Natalie",
+            age: 35,
+            favoriteClimbs: [
+                {
+                    climbName: "Denali",
+                    difficulty: 10
+                },
+            ]
+        },
+        {
+            name: "Jo",
+            age: 27,
+            favoriteClimbs: [
+                {
+                    climbName: "Mount Killimanjaro",
+                    difficulty: 10
+                },
+            ]
+        },
+        {
+            name: "Webster",
+            age: 40,
+            favoriteClimbs: [
+                {
+                    climbName: "Mount Fuji",
+                    difficulty: 10
+                },
+            ]
+            Object.college = "University of Utah",
+            Object.job = "Engineer",      
+        }
+            ]
+}
